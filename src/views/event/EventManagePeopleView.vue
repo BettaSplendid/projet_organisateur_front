22fef4e4cda9ac36181f46382b4eb4488261612d
Betta Splendid <forbiddenbetta@gmail.com>
2022-06-28 15:35:42 +0200

    Work on restrictions

--- src/views/event/EventManagePeopleView.vue
+++ src/views/event/EventManagePeopleView.vue
@@ -15,114 +15,116 @@
           <div>Application : {{ foo.application_status }}</div>
           <div>Creation Date : {{ foo.created_at }}</div>
           <div>Last Updated at : {{ foo.updated_at }}</div>
-          <div class="food_box">Food restrictions : {{ mykey.decode_and_present_restrictions(foo.food_restrictions) }}</div>
+          <div class="food_box">Food restrictions : {{ mykey.decode_and_present_restrictions(foo.food_restrictions) }}
+          </div>
+          <button @click="toggle_restr_edit()">Edit restrictions</button>
+          <div v-if="editing_restrictions">
+            Choose your diet restrictions :
+            <div>
+              <div class="diet_container">
+                <div class="diet_checkbox">
+                  <label for="milk">Milk </label>
+                  <input v-model="allergies.milk" type="checkbox" id="milk">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="meat">Meat </label>
+                  <input v-model="allergies.meat" type="checkbox" id="meat">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="gluten">Gluten </label>
+                  <input v-model="allergies.gluten" type="checkbox" id="gluten">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Eggs">Eggs </label>
+                  <input v-model="allergies.eggs" type="checkbox" id="Eggs">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Fish">Fish </label>
+                  <input v-model="allergies.fish" type="checkbox" id="Fish">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Nuts">Nuts </label>
+                  <input v-model="allergies.nuts" type="checkbox" id="Nuts">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Shellfish">Shellfish </label>
+                  <input v-model="allergies.shellfish" type="checkbox" id="Shellfish">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Soy">Soy </label>
+                  <input v-model="allergies.soy" type="checkbox" id="Soy">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Wheat">Wheat </label>
+                  <input v-model="allergies.wheat" type="checkbox" id="Wheat">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Peanut">Peanut </label>
+                  <input v-model="allergies.peanuts" type="checkbox" id="Peanut">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Poultry">Poultry </label>
+                  <input v-model="allergies.poultry" type="checkbox" id="Poultry">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Red_meat">Red meat </label>
+                  <input v-model="allergies.red_meat" type="checkbox" id="Red_meat">
+                </div>
+              </div>
+              <div class="diet_container">
+                <div class="diet_checkbox">
+                  <label for="Tree_nuts">Tree nuts </label>
+                  <input v-model="allergies.tree_nuts" type="checkbox" id="Tree_nuts">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Peanuts">Peanuts </label>
+                  <input v-model="allergies.peanuts" type="checkbox" id="Peanuts">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Sesame">Sesame </label>
+                  <input v-model="allergies.sesame" type="checkbox" id="Sesame">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Alcohol">Alcohol </label>
+                  <input v-model="allergies.alcohol" type="checkbox" id="Alcohol">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Coconut">Coconut </label>
+                  <input v-model="allergies.coconut" type="checkbox" id="Coconut">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Sugar">Sugar </label>
+                  <input v-model="allergies.sugar" type="checkbox" id="Sugar">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Sulfites">Sulfites </label>
+                  <input v-model="allergies.sulfites" type="checkbox" id="Sulfites">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="fruit">Fruit </label>
+                  <input v-model="allergies.fruit" type="checkbox" id="fruit">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Garlic">Garlic </label>
+                  <input v-model="allergies.garlic" type="checkbox" id="Garlic">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Celery">Celery </label>
+                  <input v-model="allergies.celery" type="checkbox" id="Celery">
+                </div>
+                <div class="diet_checkbox">
+                  <label for="Mustard">Mustard </label>
+                  <input v-model="allergies.mustard" type="checkbox" id="Mustard">
+                </div>
+              </div>
+
+            </div>
+
+            <button @click="mykey.encode_diet_restrictions(allergies)">Validate</button>
+          </div>
         </div>
       </div>
-      <div>
-        Choose your diet restrictions :
-        <div>
-          <div class="diet_container">
-            <div class="diet_checkbox">
-              <label for="milk">Milk </label>
-              <input v-model="allergies.milk" type="checkbox" id="milk">
-            </div>
-            <div class="diet_checkbox">
-              <label for="meat">Meat </label>
-              <input v-model="allergies.meat" type="checkbox" id="meat">
-            </div>
-            <div class="diet_checkbox">
-              <label for="gluten">Gluten </label>
-              <input v-model="allergies.gluten" type="checkbox" id="gluten">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Eggs">Eggs </label>
-              <input v-model="allergies.eggs" type="checkbox" id="Eggs">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Fish">Fish </label>
-              <input v-model="allergies.fish" type="checkbox" id="Fish">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Nuts">Nuts </label>
-              <input v-model="allergies.nuts" type="checkbox" id="Nuts">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Shellfish">Shellfish </label>
-              <input v-model="allergies.shellfish" type="checkbox" id="Shellfish">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Soy">Soy </label>
-              <input v-model="allergies.soy" type="checkbox" id="Soy">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Wheat">Wheat </label>
-              <input v-model="allergies.wheat" type="checkbox" id="Wheat">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Peanut">Peanut </label>
-              <input v-model="allergies.peanuts" type="checkbox" id="Peanut">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Poultry">Poultry </label>
-              <input v-model="allergies.poultry" type="checkbox" id="Poultry">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Red_meat">Red meat </label>
-              <input v-model="allergies.red_meat" type="checkbox" id="Red_meat">
-            </div>
-          </div>
-          <div class="diet_container">
-            <div class="diet_checkbox">
-              <label for="Tree_nuts">Tree nuts </label>
-              <input v-model="allergies.tree_nuts" type="checkbox" id="Tree_nuts">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Peanuts">Peanuts </label>
-              <input v-model="allergies.peanuts" type="checkbox" id="Peanuts">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Sesame">Sesame </label>
-              <input v-model="allergies.sesame" type="checkbox" id="Sesame">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Alcohol">Alcohol </label>
-              <input v-model="allergies.alcohol" type="checkbox" id="Alcohol">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Coconut">Coconut </label>
-              <input v-model="allergies.coconut" type="checkbox" id="Coconut">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Sugar">Sugar </label>
-              <input v-model="allergies.sugar" type="checkbox" id="Sugar">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Sulfites">Sulfites </label>
-              <input v-model="allergies.sulfites" type="checkbox" id="Sulfites">
-            </div>
-            <div class="diet_checkbox">
-              <label for="fruit">Fruit </label>
-              <input v-model="allergies.fruit" type="checkbox" id="fruit">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Garlic">Garlic </label>
-              <input v-model="allergies.garlic" type="checkbox" id="Garlic">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Celery">Celery </label>
-              <input v-model="allergies.celery" type="checkbox" id="Celery">
-            </div>
-            <div class="diet_checkbox">
-              <label for="Mustard">Mustard </label>
-              <input v-model="allergies.mustard" type="checkbox" id="Mustard">
-            </div>
-          </div>
 
-        </div>
-
-        <button @click="mykey.encode_diet_restrictions(allergies)">Validate</button>
-        <!-- <button @click="mykey.decode_diet_restrictions('1000000100010001000000')">decode 1000000100010001000000 - Milk soy sesame sulfites</button> -->
-      </div>
     </div>
     <!-- Restrictions -->
 
@@ -150,6 +152,7 @@ const route = useRoute()
 const events_store = useEventsStore()
 const user_store = useUserStore()
 
+const editing_restrictions = ref(false)
 var event_guests = ref()
 var decoded_restrictions = ref()
 const page_id = route.params.id - 1
@@ -159,6 +162,9 @@ onMounted(async () => {
   event_guests.value = await http_func.get_event_guests(route.params.id)
 })
 
+function toggle_restr_edit() {
+  editing_restrictions.value = !editing_restrictions.value
+}
 
 const restrictions_menu = ref(1)
 
@@ -186,10 +192,6 @@ const allergies = ref({
   poultry: false,
   red_meat: false,
 });
-
-// console.log(allergies.value);
-
-
 
 </script>
 
